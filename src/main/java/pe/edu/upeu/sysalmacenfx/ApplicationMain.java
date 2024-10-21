ca5cf12e3dbb88e5125e859b040c783ba0fe5b72
GONZALO <brayangonzalo1011@gmail.com>
2024-10-20 23:45:32 -0500

    hola

---
+++ src/main/java/pe/edu/upeu/sysalmacenfx/ApplicationMain.java
@@ -0,0 +1,12 @@
+package pe.edu.upeu.sysalmacenfx;
+
+import net.sf.jasperreports.engine.util.JsonUtil;
+
+public class ApplicationMain {
+    public static void main(String[] args) {
+            SysAlmacenFxApplication.main(args);
+        }
+
+    }
+
+
